e120cc2cd9a5db82c8bfcb0359e5615218bce761
Kashish Gupta <kashishgupta020899@gmail.com>
2022-07-23 23:39:43 +0530

    final site

---
+++ src/content_option.js
@@ -0,0 +1,157 @@
+const logotext = "KASHISH";
+const meta = {
+    title: "Kashish Gupta",
+    description: "I’m Kashish Gupta _ Enginerering Graduate from PEC, Chandigarh",
+};
+
+const introdata = {
+    title: "I’m Kashish Gupta",
+    animated: {
+        first: "2022 Engineering Graduate",
+        second: "I love coding and I work on Website Development",
+        third: "Working as ac Software Development Engineer",
+    },
+    description: "",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "I am an Engineering Graduate from PEC Univerity of Technology,Chandigarh who found his passion in coding.",
+};
+const Internship = [{
+        jobtitle: "Software Development Intern",
+        where: "Microsoft",
+        date: "Jan,2021-Jun,2021",
+        data: "Worked on Microsoft Teams Web Client and contributed in improving User Experience for Feeds and Notifications."
+    }
+];
+
+const skills = [{
+        name: "C++",
+        value: 90,
+    },
+    {
+        name: "OOPS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 75,
+    },
+    {
+        name: "Angular",
+        value: 75,
+    },
+    {
+        name: "MySQL",
+        value: 70,
+    }
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kashishgupta020899@gmail.com",
+    YOUR_FONE: "9671098090",
+    description: "",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/020899-Kashish",
+    linkedin: "https://www.linkedin.com/in/kashish-gupta-337375196/"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    Internship,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
